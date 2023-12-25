25dc024ee3419ff91193f0d9abc0fcb020cb7ac3
yui10 <79691301+yui10@users.noreply.github.com>
2023-12-25 21:33:35 +0900

    CORS対応

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,16 @@
 import express = require('express')
 import * as usersRouter from "./routes/users";
 import * as apiRouter from "./routes/apis/task";
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
 // CORSの設定
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    next();
-});
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    optionsSuccessStatus: 200
+}));
 
 app.use('/users', usersRouter.index);
 app.use('/api/task', apiRouter.default);
