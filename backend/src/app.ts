71cd0afd13891655aff0f0cb8ecfc89af26306f4
yui10 <79691301+yui10@users.noreply.github.com>
2024-01-25 17:23:49 +0900

    環境変数の調整

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,16 @@
 import express = require('express');
+import cors from 'cors';
 import * as usersRouter from './routes/users';
 import * as apiRouter from './routes/apis/task';
 import errorHandler, { NotFound } from './common/errorException';
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin: process.env.TRUSTED_ORIGINS,
+    credentials: true,
+    optionsSuccessStatus: 200
+}));
 
 app.use('/users', usersRouter.index);
 app.use('/api/task', apiRouter.default);
